7b447ea6b4df3142766a17d16dc1b2afcaf620d6
VJ <vijenderbakhshi@gmail.com>
2020-02-16 12:10:24 -0800

    added click input taking, have to still do the instruction 420 check and send the input off to validation which'll be removed from main loop i guess

--- word-search.lisp
+++ word-search.lisp
@@ -7,6 +7,11 @@
 (defvar *color1* (gamekit:vec4 0 0 0 1)) ;for drawing
 (defvar *color2* (gamekit:vec4 0.1 0.3 0.9 0.5))
 (defvar *color3* (gamekit:vec4 0.8 0.8 0.8 0.5))
+
+(defvar st)
+(defvar en)
+(defvar cont 420)
+(defvar st_b t)
 ;;;
 ;;; Main Class to run game
 
@@ -15,6 +20,10 @@
 	(:viewport-height *canvas-h*)
 	(:viewport-title "Word Search Game")
 
+)
+
+(defun valid(x y)
+	(and (and (< x 600) (> x -600)) (and (< y 600) (> y -600)))   
 )
 
 ;;; Bindings
@@ -27,6 +36,37 @@
 				(when *click*
 				;; put x, y position into doing something here - translate into input of currfile
 				;; INPUT GOES IN HERE 
+				
+				;; for instructions "screen"
+				(if (eq cont 420)
+					(progn (setf cont 1)
+					(return-from gamekit:post-initialize))
+				)
+				
+				;; do input things
+				(if (st_b)
+					(progn 
+						(let ((xx 600/x) (yy 600/y))
+							(format t "~% X ~a Y ~a ~%" xx yy)
+							(if (not (valid xx yy))
+								(princ ">:( Click a square within the board") ; needs to be a quick draw/flash of text
+								(setf st (list xx yy))
+							)
+						)
+					) ; true => store start
+					(progn
+						(let ((xx 600/x) (yy 600/y)
+							(format t "~% X ~a Y ~a ~%" xx yy)
+							(if (not (valid xx yy))
+								(princ ">:(( Click a square within the board") ; needs to be a quick draw/flash of text
+								(setf st (list xx yy))
+							)
+						)
+						
+						)
+					) ; false => store end
+				)
+
 				)))
 
 	(gamekit:bind-button :q :pressed (lambda () 
